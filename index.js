90accd32bca35e171dfb546c4c681998746313e8
Jason <jason@pemulis>
2016-04-11 09:44:00 -0500

    Add a style linter and make changes to satisfy it. (copied style rules from the botkit project)

--- index.js
+++ index.js
@@ -1,51 +1,54 @@
 
 if (!process.env.token) {
-  console.log ('Error: "token" expected in environment!');
-  process.exit (1);
+  console.log('Error: "token" expected in environment!');
+  process.exit(1);
 }
 
-var Botkit = require ('botkit');
-var os = require ('os');
+var Botkit = require('botkit');
+var os = require('os');
 
-var controller = Botkit.slackbot ({
-  debug:true
+var controller = Botkit.slackbot({
+  debug: true,
 });
 var bot = controller.spawn({
-  token: process.env.token
-}).startRTM ();
+  token: process.env.token,
+}).startRTM();
 
 if (process.env.youtube_key) {
-  var YouTube = require ('./lib/youtube.js');
-  YouTube.init (process.env.youtube_key, controller);
+  var YouTube = require('./lib/youtube.js');
+  YouTube.init(process.env.youtube_key, controller);
 }
 
-controller.hears (['hello', 'hi', 'sup'], 'direct_message,direct_mention,mention',
-  function (bot, message) {
+controller.hears(['hello', 'hi', 'sup'], 'direct_message,direct_mention,mention',
+  function(bot, message) {
     bot.api.reactions.add({
       timestamp: message.ts,
       channel: message.channel,
-      name: 'robot_face'
-    }, function (err, res) {
+      name: 'robot_face',
+    }, function(err, res) {
       if (err) {
-        bot.botkit.log ('Failed to add emoji reaction to "hi" message', err);
+        bot.botkit.log('Failed to add emoji reaction to "hi" message', err);
       }
     });
-});
+  }
+);
 
-controller.hears (['uptime', 'who are you', 'what is your name'],
-  'direct_message,direct_mention,mention', function (bot, message) {
-    var hostname = os.hostname ();
-    var uptime = process.uptime ();
+controller.hears(['uptime', 'who are you', 'what is your name'],
+  'direct_message,direct_mention,mention', function(bot, message) {
+    var hostname = os.hostname();
+    var uptime = process.uptime();
     bot.reply(message,
       ':robot_face: I am a bot named <@' + bot.identity.name + '>.' +
       'I have been running for ' + uptime + ' seconds on ' + hostname + '.'
     );
-});
+  }
+);
 
-controller.hears (['say:? (.*)'], 'direct_message,direct_mention,mention',
-  function (bot, message) {
+controller.hears(['say:? (.*)'], 'direct_message,direct_mention,mention',
+  function(bot, message) {
     bot.reply(message,
-      ':robot_face: ' + message.match[1] + "\n\n" +
+      ':robot_face: ' + message.match[1] + '\n\n' +
       '(Stop toying with me... I\'m just a helpless bot!)'
     );
-});
+  }
+);
